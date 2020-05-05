ee70eabc07d0035da3c95c3ce750e8726b13db2e
magda-avram <magdalena.avram@endava.com>
2020-05-05 15:02:47 +0300

    Fetch data

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,39 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React, {useState, useEffect} from "react";
+import { Text, View, FlatList, StyleSheet } from "react-native";
+
+async function getCharacters() {
+  const response = await fetch('https://swapi.dev/api/people/');
+
+  return await response.json();
+}
+
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    getCharacters().then(res => setCharacters(res.results));
+  }, []);
+
+  return (
+    <View>
+      <Text style={styles.text}>
+        Characters
+      </Text>
+      <FlatList
+        data={characters}
+        renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
+    fontSize: 30,
+  },
+  item: {
+    fontSize: 21,
   }
 });
 
